8905bbe25e0b949f1b2edf91a7c01f451a85c554
likemilktea <86944264+likemilktea@users.noreply.github.com>
2023-07-21 10:45:27 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 788 ms, Memory: 115640 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,14 +1,27 @@
-# 숫자 카드의 개수 N 입력
-N = int(input())
-# 상근이가 가지고 있는 숫자 카드 입력 및 딕셔너리로 변환
-Nlist = list(map(int, input().split()))
-card_count = {}
-for num in Nlist:
-    card_count[num] = card_count.get(num, 0) + 1
+# 문제 요약 
+# 주인공이 N장의 카드를 가지고 있다. 
+# 우리는 그 가운데 Mlist에 존재하는 카드의 갯수를 알아내야 한다.
+# 카드를 가지고 있지 않다면 0장으로 표기한다.
 
-# 쿼리 개수 M 입력
+N = int(input())
+# 리스트로 소지 카드를 입력 받는다.
+Nlist = list(map(int,input().split()))
 M = int(input())
-# 각 쿼리에 대해 결과 출력
-Mlist = list(map(int, input().split()))
-for num in Mlist:
-    print(card_count.get(num, 0), end=' ')
+# 리스트로 확인해야할 카드 넘버를 입력받는다.
+Mlist = list(map(int,input().split()))
+
+# 가진 카드별 갯수를 입력받을 딕셔너리
+ansDict = {}
+# ansDict 내부에 이미 값이 있다면 있는 값 +1
+# 없다면 0+1
+# get(a,b)의 경우 앞에는 키값,
+# 뒤에는 해당 키값이 존재하지 않는다면 0을 반환하는 함수
+for key in Nlist:
+    ansDict[key]=ansDict.get(key,0)+1
+
+# 마찬가지로 get 함수를 통해서 출력
+# asndict에 키가 있다면 해당 밸류를 
+# 없다면 0을 출력
+# 출력문 끝에 개행 대신 공백을 출력
+for key in Mlist:
+    print(ansDict.get(key,0), end = ' ')
